ff9955951f878db18981352dca79da53c1b2fa19
itishri007 <itishri2007@gmail.com>
2026-07-13 20:48:08 +0530

    Create Dbconnect.php

---
+++ 25ESKCY020/DAY 7-12/Dbconnect.php
@@ -0,0 +1,14 @@
+<?php
+$host     = "localhost";
+$user     = "root";
+$password = "";
+$database = "industrila_training";
+
+$conn= mysqli_connect($host, $user, $password, $database);
+
+if (!$conn) {
+    die("Connection Failed: " . mysqli_connect_error());
+}
+
+echo "Connection Successful!";
+?>
